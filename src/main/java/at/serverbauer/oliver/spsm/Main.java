0aba35bbc50d5061c513cdd96b10544aa12fa617
Serverbauer <hessfinn@gmail.com>
2022-12-11 21:18:29 +0100

    Repository Creation

---
+++ src/main/java/at/serverbauer/oliver/spsm/Main.java
@@ -0,0 +1,17 @@
+package at.serverbauer.oliver.spsm;
+
+/**
+ * JavaDoc this file!
+ * Created: 26.01.2021
+ * 13:54
+ * 26.01.2021
+ * <p>
+ * 13:54
+ * <p>
+ * SPSM_Windows
+ * at.serverbauer.oliver.spsm
+ *
+ * @author Serverbauer | GermanRPGBrothers.eu Inhaber
+ */
+public class Main {
+}
