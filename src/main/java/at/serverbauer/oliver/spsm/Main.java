a46504c3dd63adebb81c778e7b18120da28f65a2
Serverbauer <hessfinn@gmail.com>
2022-12-11 21:50:45 +0100

    First version
    getestet ohne Socket also normal localhost mit einer System.out.println(""); wiedergabe

--- src/main/java/at/serverbauer/oliver/spsm/Main.java
+++ src/main/java/at/serverbauer/oliver/spsm/Main.java
@@ -1,5 +1,15 @@
 package at.serverbauer.oliver.spsm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.sun.management.OperatingSystemMXBean;
+
 /**
  * JavaDoc this file!
  * Created: 26.01.2021
@@ -11,7 +21,45 @@ package at.serverbauer.oliver.spsm;
  * SPSM_Windows
  * at.serverbauer.oliver.spsm
  *
- * @author Serverbauer | GermanRPGBrothers.eu Inhaber
+ * @author Serverbauer | GermanRPGBrothers.eu Inhaber and Oliver
  */
+
 public class Main {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            // Bean für den Betriebssystemmanager abrufen
+            OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+            while (true) {
+                // Warten auf eingehende Verbindungen
+                Socket clientSocket = serverSocket.accept();
+
+                // Streams für die Kommunikation mit dem Client erstellen
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                // Nachricht vom Client empfangen
+                String request = in.readLine();
+
+                // CPU-Auslastung ermitteln
+                double cpuLoad = osBean.getSystemCpuLoad();
+
+                // RAM-Auslastung ermitteln
+                long totalMemory = osBean.getTotalPhysicalMemorySize();
+
+                // Disk-Auslastung ermitteln
+                long freeDiskSpace = osBean.getFreePhysicalMemorySize();
+
+                // Netzwerk-Auslastung ermitteln
+                long totalNetwork = osBean.getTotalSwapSpaceSize();
+
+                // Antwort an den Client senden
+                out.println("Der request wurde empfangen. Die CPU-Auslastung beträgt: " + cpuLoad + " Die RAM-Auslastung beträgt: " + totalMemory + " Die Disk-Auslastung beträgt: " + freeDiskSpace + " Die Netzwerk-Auslastung beträgt: " + totalNetwork);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
